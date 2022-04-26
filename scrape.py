5806a99565e944e2a5ff323f96f5fe5f06bac2f3
Allan Whatmough <al.whatmough@gmail.com>
2022-04-26 19:57:54 +1000

    Update scrape.py

---
+++ scrape.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import sync_playwright
+
+playwright = sync_playwright().start()
+
+browser = playwright.chromium.launch()
+
+page = browser.new_page()
+
+page.goto("https://daily.al.run/")
+
+break_out_flag = False
+for post in page.query_selector_all('p'):
+  f = open('foo.txt', 'w')
+  url = post.eval_on_selector("a", "el => el.href")
+  page2 = browser.new_page()
+  page2.goto(url) 
+  for para in page2.query_selector_all("p"):
+    f.write(para.inner_html())
+    break_out_flag = True
+    break
+  if break_out_flag:
+    break
+
+browser.close()
+
+playwright.stop()
